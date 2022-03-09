54a5b8253130a8e904e6a73cf176de601fcb02e6
안형민(An, hyoung-min) <wakehm@gmail.com>
2022-03-10 00:05:27 +0900

    mattermost 분리

---
+++ src/mattermost.groovy
@@ -0,0 +1,19 @@
+void testMethod(
+    String url,
+    String channel,
+    String username,
+    String text,
+    String icon='https://www.mattermost.org/wp-content/uploads/2016/04/icon.png',
+  ) {
+    httpRequest contentType: 'APPLICATION_JSON_UTF8',
+        httpMode: 'POST',
+        requestBody: "{ \
+            \"channel\": \"${channel}\", \
+            \"username\": \"${username}\", \
+            \"icon_url\": \"${icon}\", \
+            \"text\": \"${text}\" \
+        }",
+        responseHandle: 'NONE',
+        url: "${url}",
+        wrapAsMultipart: false
+}
